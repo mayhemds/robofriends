5527e8eacf254410b6546f53133c251ed09bdc51
Tyrone May <tyronemay@Tyrones-MacBook-Pro.local>
2019-08-21 17:10:35 +0100

    Updates

---
+++ precache-manifest.5faf805edbfd872228c60de63d4a27f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55b00ff04a99bbaf63a07ea8972cbc9e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b0aa47318ebbc6669c80",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "178f4dfbcdcafc92d21b",
+    "url": "/robofriends/static/css/main.d6a1f33e.chunk.css"
+  },
+  {
+    "revision": "b0aa47318ebbc6669c80",
+    "url": "/robofriends/static/js/2.da4532ac.chunk.js"
+  },
+  {
+    "revision": "178f4dfbcdcafc92d21b",
+    "url": "/robofriends/static/js/main.004715da.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  }
+]);
